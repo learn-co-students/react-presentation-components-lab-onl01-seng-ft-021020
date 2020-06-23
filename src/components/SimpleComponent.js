2c324ac836815d9bfdbf9ee16d4e35362f555170
93geraldtee <93geraldtee@gmail.com>
2020-06-23 00:25:45 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,32 @@
 // Code SimpleComponent Here
+
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            mood: "happy"
+        }
+    }
+
+    handleClick = () => {
+        if (this.state.mood === "happy") {
+            this.setState({mood: 'sad'})
+        } else {
+            this.setState({mood: 'happy'})
+        }
+        // const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+        // this.setState({ mood: newMood });
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
